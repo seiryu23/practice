eed2f365f33028c796c741bb54c6c5a39a733ed1
seiryu23 <ryu.mu.ply@icloud.com>
2023-02-02 20:15:21 +0900

    2023.02.02 Chg

--- src/components/navigate/Navigate.js
+++ src/components/navigate/Navigate.js
@@ -1,80 +1,52 @@
-import React from 'react';
-import { response } from 'express';
+import React, { useEffect, useState } from 'react';
+import * as express from 'express';
 import './Navigate.css';
-//import 'mysql2';
-//import 'express';
-//
+import * as mysql from 'mysql2';
+
+const app = express();
+const port = 3000;
+
 function Navigate() {
-    SQL();
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        SQL();
+    }, []);
+    
     return (
         <nav className="nav">
-            <a>aaa</a>
+            <a>{data}</a>
         </nav>
     );
-}
 
-function SQL()
-{
-    const express = require('express');
-    const app = express();
-    const port = 3000;
+    async function SQL()
+    {
+        app.get('/', (req, response) => response.send('Hello World!'));
 
-    app.get('/', (req, response) => response.send('Hello World!'));
-
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-    /*
-    const express = require('express');
-    const app = express();
-//    const port = 3000;
-    var conn = require('mysql2').createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Lbmz0204!',
-        database: 'us_angular'
-    });
-    conn.connect(function(err) {
-        if (err) throw err;
-        console.log('Connected');
-    });
-    
-    app.get('/', (request, response) => {
-        const sql = "select * from userinf"
-        conn.query(sql, function (err, result, fields) {  
-        if (err) throw err;
-        response.send(result)
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+        
+        const conn = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'Lbmz0204!',
+            database: 'us_angular'
         });
-    });
-    */
+        conn.connect(function(err) {
+            if (err) throw err;
+            console.log('Connected');
+        });
+        
+        app.get('/', async (request, response) => {
+            const sql = "select * from userinf";
+            const result = await new Promise((resolve, reject) => {
+                conn.query(sql, function (err, result, fields) {  
+                    if (err) reject(err);
+                    resolve(result);
+                });
+            });
+            setData(JSON.stringify(result));
+        });
+    }
 }
-
-/* 
-// SQL Server使用時の処理
-function GetMdcAdvic() {
-    var Connection = require('tedious').Connection;  
-    var config = {  
-        server: '(local)',  //update me
-        authentication: {
-            type: 'default',
-            options: {
-                userName: 'sa', //update me
-                password: 'itec'  //update me
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'DEV_V6Uehata'  //update me
-        }
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
-        console.log("Connected");  
-    });
-    
-    connection.connect();
-}
-*/
-
 
 export default Navigate;
